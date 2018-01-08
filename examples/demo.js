59cd403fe250d9bf61878a1ab483b9a2a90dc213
Jeff Hoefs <jeff.hoefs@gmail.com>
2018-01-07 20:44:12 -0800

    separate fields for dependency name and class name

--- examples/demo.js
+++ examples/demo.js
@@ -21,6 +21,9 @@ var simulatedUserInput = {
     "StepperFirmata",
     "AccelStepperFirmata",
     "SerialFirmata",
+    "RCOutputFirmata",
+    "RCInputFirmata",
+    "FirmataEncoder",
     "FirmataScheduler"
   ]
 };
